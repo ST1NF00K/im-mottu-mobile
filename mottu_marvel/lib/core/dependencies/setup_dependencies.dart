bb9326562759e3c55cf843554ef13e80b05016c5
ST1NF00K <liviacastilholi@gmail.com>
2024-09-03 21:27:51 -0300

    setup dependencies with get_it

---
+++ mottu_marvel/lib/core/dependencies/setup_dependencies.dart
@@ -0,0 +1,25 @@
+import 'package:get_it/get_it.dart';
+import '../../app/characters/controller/characters_list_controller.dart';
+import '../../app/characters/repository/character_repository.dart';
+import '../api/http_service/dio/dio_http_service.dart';
+import '../api/http_service/http_service.dart';
+
+final GetIt getIt = GetIt.instance;
+
+void setupDependencies() {
+  getIt.registerLazySingleton<HttpService>(
+    () => DioHttpService(),
+  );
+
+  getIt.registerLazySingleton<CharacterRepository>(
+    () => CharacterRepository(
+      http: getIt.get<HttpService>(),
+    ),
+  );
+
+  getIt.registerFactory<CharactersListController>(
+    () => CharactersListController(
+      repository: getIt.get<CharacterRepository>(),
+    ),
+  );
+}
